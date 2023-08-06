e1e386142aed08d64a1b0438803f906597cd1c1d
naledi-dikgale <naledi22khosi@gmail.com>
2023-08-06 10:27:59 +0200

    Setup empty project

--- app/models/category.rb
+++
@@ -1,5 +0,0 @@
-class Category < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :category_items, dependent: :destroy
-  has_many :items, through: :category_items
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "categories#index"
-  resources :categories, only: [:index, :new, :show, :create] do
-    resources :items, only: [:index, :new, :create]
-  end
-end

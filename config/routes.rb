674303314c8854f2558a13eb5cc5d8f631d0d10f
naledi-dikgale <naledi22khosi@gmail.com>
2023-08-06 22:20:04 +0200

    Add splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "categories#index"
+  root "splash_screen#index"
   resources :categories, only: [:index, :new, :show, :create] do
     resources :items, only: [:index, :new, :create]
   end

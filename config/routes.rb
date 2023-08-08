3e687290cef16b171908eea6e586e0b40dac9677
naledi-dikgale <naledi22khosi@gmail.com>
2023-08-08 10:26:20 +0200

    Update styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "splash_screen#index"
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+  unauthenticated do
+    root "splash_screen#index", as: :unauthenticated_root
+  end
+  # root "splash_screen#index"
   resources :categories, only: [:index, :new, :show, :create] do
     resources :items, only: [:index, :new, :create]
   end
